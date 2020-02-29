68efb6b8f72f4d1ff26116dd1bd87e64abdfe760
Vaibhav <vamdigitalltd@gmail.com>
2020-02-29 13:57:44 +0000

    Add sample selector file@

--- src/redux/selector/sample.js
+++ src/redux/selector/sample.js
@@ -2,9 +2,6 @@ import { createSelector } from 'reselect'
 
 const sample = state => state.data
 
-const getCount = createSelector(
-  sample,
-  item => item.count
-)
+const getCount = createSelector(sample, item => item.count)
 
 export default getCount
