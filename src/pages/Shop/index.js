8f8707cd6ff7d5a0bbfe0a653201f213da648a0e
Vaibhav <vamdigitalltd@gmail.com>
2020-02-29 22:26:18 +0000

    Added styles and static layout for Shop page view

--- src/pages/Shop/index.js
+++ src/pages/Shop/index.js
@@ -1,10 +1,119 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
+import Button from '../../components/Button'
+import './shop.scss'
 
 const Shop = () => {
   return (
-    <div>
-      <h1>Shop</h1>
-    </div>
+    <section className="page-section" id="shop">
+      <div className="container">
+        <h1 className="shop-title">The Collection</h1>
+        <h2 className="shop-blurb">
+          Leave your mark on every occasion, whatever your style. A decisive step to give
+          personality and uniqueness to your fashion spirit. A selection that will allow you to find
+          the right choice for your outfit: elegant, casual or smart, Dance Naturals will walk with
+          you.
+        </h2>
+        <ul className="card">
+          <li className="card-item">
+            <div className="card-content">
+              <img src="https://i.ibb.co/dg2jdjk/1.jpg" alt="Fashion" />
+              <p className="product-title">Skinny mid-rise Trousers</p>
+              <p className="product-price">£130.00</p>
+              <Button buttonText="Add to Cart" buttonType="button" buttonVariant="primary" />
+            </div>
+          </li>
+          <li className="card-item">
+            <div className="card-content">
+              <img src="https://i.ibb.co/dg2jdjk/1.jpg" alt="Fashion" />
+              <p className="product-title">Skinny mid-rise Trousers</p>
+              <p className="product-price">£130.00</p>
+              <Button buttonText="Add to Cart" buttonType="button" buttonVariant="primary" />
+            </div>
+          </li>
+          <li className="card-item">
+            <div className="card-content">
+              <img src="https://i.ibb.co/dg2jdjk/1.jpg" alt="Fashion" />
+              <p className="product-title">Skinny mid-rise Trousers</p>
+              <p className="product-price">£130.00</p>
+              <Button buttonText="Add to Cart" buttonType="button" buttonVariant="primary" />
+            </div>
+          </li>
+          <li className="card-item">
+            <div className="card-content">
+              <img src="https://i.ibb.co/dg2jdjk/1.jpg" alt="Fashion" />
+              <p className="product-title">Skinny mid-rise Trousers</p>
+              <p className="product-price">£130.00</p>
+              <Button buttonText="Add to Cart" buttonType="button" buttonVariant="primary" />
+            </div>
+          </li>
+          <li className="card-item">
+            <div className="card-content">
+              <img src="https://i.ibb.co/dg2jdjk/1.jpg" alt="Fashion" />
+              <p className="product-title">Skinny mid-rise Trousers</p>
+              <p className="product-price">£130.00</p>
+              <Button buttonText="Add to Cart" buttonType="button" buttonVariant="primary" />
+            </div>
+          </li>
+          <li className="card-item">
+            <div className="card-content">
+              <img src="https://i.ibb.co/dg2jdjk/1.jpg" alt="Fashion" />
+              <p className="product-title">Skinny mid-rise Trousers</p>
+              <p className="product-price">£130.00</p>
+              <Button buttonText="Add to Cart" buttonType="button" buttonVariant="primary" />
+            </div>
+          </li>
+          <li className="card-item">
+            <div className="card-content">
+              <img src="https://i.ibb.co/dg2jdjk/1.jpg" alt="Fashion" />
+              <p className="product-title">Skinny mid-rise Trousers</p>
+              <p className="product-price">£130.00</p>
+              <Button buttonText="Add to Cart" buttonType="button" buttonVariant="primary" />
+            </div>
+          </li>
+          <li className="card-item">
+            <div className="card-content">
+              <img src="https://i.ibb.co/dg2jdjk/1.jpg" alt="Fashion" />
+              <p className="product-title">Skinny mid-rise Trousers</p>
+              <p className="product-price">£130.00</p>
+              <Button buttonText="Add to Cart" buttonType="button" buttonVariant="primary" />
+            </div>
+          </li>
+          <li className="card-item">
+            <div className="card-content">
+              <img src="https://i.ibb.co/dg2jdjk/1.jpg" alt="Fashion" />
+              <p className="product-title">Skinny mid-rise Trousers</p>
+              <p className="product-price">£130.00</p>
+              <Button buttonText="Add to Cart" buttonType="button" buttonVariant="primary" />
+            </div>
+          </li>
+          <li className="card-item">
+            <div className="card-content">
+              <img src="https://i.ibb.co/dg2jdjk/1.jpg" alt="Fashion" />
+              <p className="product-title">Skinny mid-rise Trousers</p>
+              <p className="product-price">£130.00</p>
+              <Button buttonText="Add to Cart" buttonType="button" buttonVariant="primary" />
+            </div>
+          </li>
+          <li className="card-item">
+            <div className="card-content">
+              <img src="https://i.ibb.co/dg2jdjk/1.jpg" alt="Fashion" />
+              <p className="product-title">Skinny mid-rise Trousers</p>
+              <p className="product-price">£130.00</p>
+              <Button buttonText="Add to Cart" buttonType="button" buttonVariant="primary" />
+            </div>
+          </li>
+          <li className="card-item">
+            <div className="card-content">
+              <img src="https://i.ibb.co/dg2jdjk/1.jpg" alt="Fashion" />
+              <p className="product-title">Skinny mid-rise Trousers</p>
+              <p className="product-price">£130.00</p>
+              <Button buttonText="Add to Cart" buttonType="button" buttonVariant="primary" />
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
   )
 }
 
