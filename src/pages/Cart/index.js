a4ad9b521b553f8a9fb7af3ad487dfbd489ad831
Vaibhav <vamdigitalltd@gmail.com>
2020-03-24 23:44:24 +0000

    Adds Cart Page - Initial

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <section className="page-section" id="cart">
+      <div className="container">
+        <h1>Cart</h1>
+      </div>
+    </section>
+  )
+}
+
+export default Cart
