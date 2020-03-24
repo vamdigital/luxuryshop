6cfc379fbaef3be55c92f51c68c7c432839757aa
Vaibhav <vamdigitalltd@gmail.com>
2020-03-24 23:42:39 +0000

    Adds redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import logger from 'redux-logger'
 import rootReducer from './reducers'
 
-const middleware = applyMiddleware(thunk, logger)
+const middlewares = [thunk]
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger)
+}
+const middleware = applyMiddleware(...middlewares)
 
 const store = createStore(rootReducer, composeWithDevTools(middleware))
 
